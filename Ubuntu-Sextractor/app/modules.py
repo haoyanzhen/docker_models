ab92a7af6619b7974e3687dae18eb275d0bc4f1b
haoyanzhen <haoyanzhen@shao.ac.cn>
2024-08-19 23:24:24 +0800

    update: docker: ubuntu-sextractor

---
+++ Ubuntu-Sextractor/app/modules.py
@@ -0,0 +1,6 @@
+import numpy as np
+from astropy.io import fits
+from astropy import table
+
+def block():
+    pass
---
+++ Ubuntu-Sextractor/app/run.py
@@ -0,0 +1,5 @@
+import os
+import sys
+from modules import *
+
+print("run success!")
